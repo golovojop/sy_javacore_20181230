cb1ac7f181d7798649e8f85177d64a7f206c15af
ciscoff <ciscoff@mail.ru>
2019-01-04 11:52:43 +0300

    Подсчет повторений слов в массиве

--- src/lesson3/Main.java
+++ src/lesson3/Main.java
@@ -1,8 +1,41 @@
 package lesson3;
 
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Happy new year !");
+
+        String[] data = {
+                "Oracle",
+                "RedHat",
+                "Java",
+                "Hello",
+                "world",
+                "OpenJDK",
+                "world",
+                "Java",
+                "Linux",
+                "Blockchain",
+                "Oracle",
+                "Java",
+                "Override"
+        };
+
+        ArrayList<String> al = new ArrayList<>(Arrays.asList(data));
+        System.out.println("Слов в исходном массиве: " + al.size());
+
+        // Количество уникальных слов в исходном массиве
+        TreeSet<String> ts = new TreeSet<>(al);
+        System.out.println("\nУникальных слов: " + ts.size());
+        System.out.println(ts);
+
+        // Количество повторений каждого слова
+        System.out.println("\nКоличество повторений:");
+        for(String s : ts){
+            System.out.println(s + " -> " + Collections.frequency(al, s));
+        }
+
+
     }
 }
